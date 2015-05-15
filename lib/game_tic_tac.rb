5329ae467f854c927a6bde345ded714468683d98
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-14 22:06:43 -0500

    game passed w/player 1 win, need to test further

--- lib/game_tic_tac.rb
+++ lib/game_tic_tac.rb
@@ -10,7 +10,16 @@ class Game
     @board = Board.new
     # @rule = Rule.new
     @winner = ""
-    @out_of_moves = false
+    @out_of_moves = 0
+    @win_array = [[0,1,2],
+               [3,4,5],
+               [6,7,8],
+               [0,4,8],
+               [2,4,6],
+               [0,3,6],
+               [1,4,7],
+               [2,5,8]]
+    @counter = 0
   end
 
   def start
@@ -21,12 +30,13 @@ class Game
     puts ""
 
     # begin loop if no winner
-    #while @winner == "" || !@out_of_moves
+    while @winner == "" && @out_of_moves < 9
 
       # asker player 1
       puts "- Player One your turn - "
-      print "- Select 1 thru 9 - "
+      print "- Select 1 thru 9 : "
       player_one_move = gets.chomp
+      @out_of_moves += 1
       puts ""
 
       # store player 1 move
@@ -39,29 +49,37 @@ class Game
       @board.display(@board.current_board)
       puts ""
 
-      # asker player 2
-      puts "- Player Two your turn - "
-      print "- Select 1 thru 9 - "
-      player_two_move = gets.chomp
-      puts ""
+      #check for winner
+      # @rule.
+      did_anyone_win(@player_one.name, @player_one.moves)
 
-      #store player 2 move
-      store_p2_move(player_two_move)
+      #if no winner continue & there are still moves left
+      # if @winner = "" && @out_of_moves < 9
+      break if @winner != "" || @out_of_moves >= 9
+        # asker player 2
+        puts "- Player Two your turn - "
+        print "- Select 1 thru 9 : "
+        player_two_move = gets.chomp
+        @out_of_moves += 1
+        puts ""
 
-      # update board
-      update_board(@player_two.moves.last, @player_two.token)
+        #store player 2 move
+        store_p2_move(player_two_move)
 
-      # display board
-      @board.display(@board.current_board)
-      puts ""
+        # update board
+        update_board(@player_two.moves.last, @player_two.token)
 
-      # check for winner
-      #@rule.did_anyone_win(@player_one.name, @player_one.moves)
+        # display board
+        @board.display(@board.current_board)
+        puts ""
 
-
-    #end loop
-
-    if @winner != ""
+        # check for winner
+        #@rule.
+        did_anyone_win(@player_two.name, @player_two.moves)
+      # end # no winner & moves left cond
+    end # while loop
+    puts "#{@winner} shown as winner" # this is to debug
+    if @winner != "" || @out_of_moves >= 9
       display_winner
     end # if winner condition
   end # start method
@@ -97,4 +115,29 @@ class Game
       puts "...Looks like we have a tie..."
     end
   end # display method
+
+  def did_anyone_win(player_name, player_moves) # if player's positions match in any of these positions they win
+
+    @win_array.each do |mini_array| # look at each mini array within win_array
+      @counter = 0 # Initialize counter each time you look at a mini array
+
+      mini_array.each do |win_pos| # look at each element in mini array
+
+        # loop thru player array compare each individual position
+        # to number in array
+        player_moves.each do |position|
+
+          if position == win_pos #if they match add one to counter
+            @counter += 1
+
+          end # count up cond
+        end # 3rd loop
+      end # 2nd loop
+      puts "#{player_name} #{@counter} #{@winner}" #this is to debug
+      if @counter == 3 # if three matches while looking at a mini array, player has won
+        @winner = player_name
+        puts "#{@counter} at time winner is selected" #this is to debug
+      end # winner cond
+    end # 1st loop
+  end # method
 end # Game Class
--- lib/rule.rb
+++ lib/rule.rb
@@ -1,48 +1,42 @@
 class Rule
 
-def initialize
- @win_array = [[0,1,2],
-             [3,4,5],
-             [6,7,8],
-             [0,4,8],
-             [2,4,6],
-             [0,3,6],
-             [1,4,7],
-             [2,5,8]]
-end
+  def initialize
+   @win_array = [[0,1,2],
+               [3,4,5],
+               [6,7,8],
+               [0,4,8],
+               [2,4,6],
+               [0,3,6],
+               [1,4,7],
+               [2,5,8]]
+    @counter = 0
+  end
 
 
-# def did_anyone_win(@player.name, @player.moves) #if X in any of these positions X wins
+  def did_anyone_win(player_name, player_moves) # if player's positions match in any of these positions they win
 
-  # @win_array.each do |mini_array| #look at each mini array in win_array
-    # counter = 0
+    @win_array.each do |mini_array| # look at each mini array within win_array
+      # counter = 0 # Initialize counter each time you look at a mini array
 
-    # mini_array.each do |win_pos| #look at each element in mini array
+      mini_array.each do |win_pos| # look at each element in mini array
+        @counter = 0
+        # loop thru player array compare each individual position
+        # to number in array
+        player_moves.each do |position|
 
-      # loop thru player array compare each individual position
-      # to number in array
-      # @player.moves.each do |position|
+          if position == win_pos #if they match add one to counter
+            @counter += 1
 
-        # if @player.moves == win_pos #if they match add one to counter
-          #counter += 1
-        #end - count up cond
-      #end - 3rd loop
-    #end - 2nd loop
+          end # count up cond
+        end # 3rd loop
+      end # 2nd loop
+      puts "#{player_name} #{@counter}" #this is to debug
+      if @counter == 3 # if three matches while looking at a mini array, player has won
+        @winner = player_name
+        puts "#{@counter} at time winner is selected" #this is to debug
+      end # winner cond
 
-    #if counter == 3
-      #@winner = player_name
-    #end - counter cond
+    end # 1st loop
+  end # method
+end # class
 
-  #end - 1st loop
-#end - method
-
-
-
-
-
-
-
-
-
-
-# if O in any of these positions 0 wins
